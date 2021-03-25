0573c971f08449a04ffcd23a0bcc0c3927b283db
BenBlu <benblumenfeld2001@gmail.com>
2021-03-26 00:13:04 +0200

    Added Joystick.xaml.cs file and editted it a bit

---
+++ AdvancedProgrammingProject1/Views/Joystick.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace AdvancedProgrammingProject1.Views
+{
+    public class JoystickEventArgs
+    {
+        private double x;
+        private double y;
+        public JoystickEventArgs()
+        {
+            x = 0;
+            y = 0;
+        }
+        public double X
+        {
+            get { return X; }
+            set
+            {
+                x = value;
+            }
+        }
+        public double Y
+        {
+            get { return y; }
+            set
+            {
+                y = value;
+            }
+        }
+    }
+}
