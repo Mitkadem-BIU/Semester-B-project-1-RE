8c647463dc9f4f9c01a8235e66e64af8250d63f9
BenBlu <benblumenfeld2001@gmail.com>
2021-04-01 20:23:11 +0300

    added some features to the control bar view

---
+++ AdvancedProgrammingProject1/Views/ControlBarView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AdvancedProgrammingProject1.Views
+{
+    public partial class ControlBarView: UserControl
+    {
+        public ControlBarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
