e2ebb2bd752d981c315b83371223d207d2048351
David Galustov <david.galustov@live.biu.ac.il>
2021-04-06 13:44:08 +0300

    chages

--- AdvancedProgrammingProject1/App.xaml.cs
+++ AdvancedProgrammingProject1/App.xaml.cs
@@ -24,7 +24,8 @@ namespace AdvancedProgrammingProject1
         {
             Model = new MainControllerModel();
             MainVM = new MainControllerViewModel(Model);
-            APVM = new AttrPlotViewModel(Model);
+            APVM = new AttrPlotViewModel(Model.AP);
+            CBVM = new ControlBarViewModel(Model);
             // Create main application window
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
