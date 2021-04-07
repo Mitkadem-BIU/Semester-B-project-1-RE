5f6beb5d3f1401743791e04eedcd9b0b287229b0
Tomer Aharoni <tomer.aharoni@live.biu.ac.il>
2021-04-07 13:52:03 +0300

    start of joustick

---
+++ AdvancedProgrammingProject1/ViewModels/JoystickBoardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedProgrammingProject1.ViewModels
+{
+    class JoystickBoardViewModel
+    {
+        double elevator = 0;
+        double aileron = 0;
+        double rudder = 0;
+        double throttle = 0;
+    }
+}
