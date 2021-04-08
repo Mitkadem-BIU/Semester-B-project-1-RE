3154148efda89fc438970d6839690b0606494173
bueinat <62245924+bueinat@users.noreply.github.com>
2021-04-08 20:08:36 +0300

    points show on regression line

---
+++ AdvancedProgrammingProject1/Models/StatisticMethods.cs
@@ -0,0 +1,48 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedProgrammingProject1
+{
+    class StatisticMethods
+    {
+        public static double Variance(List<double> x)
+        {
+            double sqsum = 0;
+            foreach (double val in x)
+            {
+                sqsum += val * val;
+            }
+            return sqsum / x.Count() - x.Average() * x.Average();
+        }
+
+        public static double Pearson(List<double> x, List<double> y)
+        {
+            return Covariance(x, y) / Math.Sqrt(Variance(x) * Variance(y));
+        }
+
+        public static LineSeries LinearReg(List<double> x, List<double> y)
+        {
+            double a = Covariance(x, y) / Variance(x);
+            double b = y.Average() - a * x.Average();
+            LineSeries LS = new LineSeries();
+            double lowerLim = Math.Min(x.Min() * 0.9, x.Min() * 1.1);
+            double upperLim = Math.Max(x.Max() * 0.9, x.Max() * 1.1);
+            for (double i = lowerLim; i <= upperLim; i += 0.1)
+                LS.Points.Add(new DataPoint(i, a * i + b));
+            return LS;
+        }
+
+        public static double Covariance(List<double> x, List<double> y)
+        {
+            List<double> covarr = new List<double>();
+            for (int i = 0; i < x.Count(); i++)
+                covarr.Add((x[i] - x.Average()) * (y[i] - y.Average()));
+            return covarr.Average();
+        }
+    }
+}
