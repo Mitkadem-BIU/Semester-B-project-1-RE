a97b037a8d053a2c2fc226a0cb3a6989580adfb6
David Galustov <david.galustov@live.biu.ac.il>
2021-04-10 00:13:46 +0300

    final fix

--- AdvancedProgrammingProject1/Models/JoystickModel.cs
+++ AdvancedProgrammingProject1/Models/JoystickModel.cs
@@ -20,7 +20,13 @@ namespace AdvancedProgrammingProject1
 
         public double Elevator
         {
-            get { return Model.Elevator*125 -90; }
+            get
+            {
+               
+                    return -(Model.Elevator * 125) - 90;
+                
+             
+            }
             set { Model.Elevator = value; }
         }
 
