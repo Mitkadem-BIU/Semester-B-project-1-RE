e30e6e6ac580d33ed4a9219b57ed09dceb4ae701
bueinat <62245924+bueinat@users.noreply.github.com>
2021-04-11 18:04:19 +0300

    added detection algorithm interface

---
+++ AdvancedProgrammingProject1/Models/IAlgorithm.cs
@@ -0,0 +1,28 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedProgrammingProject1
+{
+    /*
+     * The purpose of this interface is to show what abilities and properties
+     * the anomaly detection have to satisfy.
+     */
+    interface IAlgorithm
+    {
+        PlotModel GetGraphicalView();
+        Dictionary<double, Anomaly> GetAnomalies();
+    }
+
+    class Anomaly // you can inherit this class and add more functionalities to it
+    {
+        string description;
+        public string Description
+        {
+            get; set;
+        }
+    }
+}
